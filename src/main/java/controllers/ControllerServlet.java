7a947a95a9d15f1b7f1458777515ae19b4959281
NguyenTienDat-GTR <tiendat.107001@gmail.com>
2023-09-09 09:04:28 +0700

    1

--- src/main/java/controllers/ControllerServlet.java
+++ src/main/java/controllers/ControllerServlet.java
@@ -1,4 +1,20 @@
 package controllers;
 
-public class ControllerServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"ControllerServlet"})
+public class ControllerServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        String action = req.getParameter("action");
+
+    }
+    protected  void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        String action = req.getParameter("action");
+    }
 }
